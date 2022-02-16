ff431948c13704b2b7c0ecaa3a210bcf8f72422e
alv-314 <alicov314@gmail.com>
2022-02-16 14:47:20 -0500

    fixed now fr fr

--- Programming Assignment 0/part2/pqueue.c
+++ Programming Assignment 0/part2/pqueue.c
@@ -15,119 +15,111 @@ You need to submit the C program containing you test cases along the heap implem
 pqueue* create_pq(int init)
 {
     pqueue* newpq = (pqueue*)malloc(sizeof(pqueue));
-
-    //initializes first element in queue
+    //initializes first element in pq
     newpq->size = 1;
-    newpq->mHeap[0] = init;
+    newpq->m_heap[0] = init;
     return newpq;
 }
 
-//simple swap method
-void swap(int* one, int* two)
-{
-    int temp = *two;
-    *two = *one;
-    *one = temp;
-}
 //returns size of the pqueue
 int size(pqueue* pq)
 {
     return pq->size;
 }
-//n is the size of the elements 
-//i is the index which is being percolated up
-void percolate_up(pqueue* pq, int n, int i)
-{
-    int index, parent;
-    
-    index = i;
-    parent = (index-1)/2; 
-    while(pq->mHeap[index] > pq->mHeap[parent] && index > 0 && parent >= 0)
-    {
-        //swap takes place
-        swap(&(pq->mHeap[index]), &(pq->mHeap[parent]));
-        //index becomes the parent for the next loop
-        index = parent;
-        //parent is of parent calculated for next loop
-        parent = (index-1)/2;
-    }
+
+//methods to obtain the parent, the left, and the right nodes
+int parentNode(int i){
+    return ((i-1)/2); 
 }
-//n = size of the array
-//i = node at which to heapify
+
+int leftNode(int i){
+    return ((2*i)+1);
+}
+
+int rightNode(int i){
+    return ((2*i)+2);
+}
+
+//simple swap method
+void swap(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void percolate_down(pqueue* pq, int n, int i)
 {
-    int index, temp, left, right;
-    index = i;
-    temp = i;
-    left = 2 * i + 1; //index left child
-    right = 2 * i + 2; //index right child
-
-    //condition 1 checks that left and right are within the heap size
-    //condition 2 checks that either of the children are bigger than the parent
-    while(right < n && left < n && (pq->mHeap[i] < pq->mHeap[left]|| pq->mHeap[i] < pq->mHeap[right]))
-    {
-        //finding biggest of i's parents
-        if(pq->mHeap[left] > pq->mHeap[right])temp = left;
-        else temp = right;
-        swap(&(pq->mHeap[temp]), &(pq->mHeap[i]));
-        //recalculate parent and children
-        index = temp;
-        left = 2 * temp + 1;
-        right = 2 * temp + 2;
+    int max, left, right;
+    max = 0;
+    left = leftNode(i);
+    right = rightNode(i);    
+    //conditions
+    //child is within size
+    //child is bigger than parent
+    //find bigger of the two children
+    if(right < n && pq->m_heap[right] > pq->m_heap[i] && pq->m_heap[right] > pq->m_heap[left]){
+        max = right;
+    }else if(left < n && pq->m_heap[left] > pq->m_heap[i]){
+        max = left;
+    }else{
+        //neither child is bigger, stop
+        return;
     }
+
+    //swap because it didn't return
+    swap(&pq->m_heap[i],&pq->m_heap[max]);
+    //percolate down at next children
+    percolate_down(pq,size(pq),max);
 }
 
 void add(pqueue* pq, int val)
-{
-    if(size(pq) == 1000){
-        printf("Cannot add more to the heap!");
-        return;
-    }
-    if(size(pq) == 0){
-        pq->mHeap[0] = val;
-        pq->size++;
-        return;
-    }
-    pq->mHeap[size(pq)] = val;
-    pq->size++;
+{   
+    int i = size(pq);
+    pq->m_heap[i] = val;
     
-    //added to the bottom so it needs to be percolated up
-    percolate_up(pq, size(pq), size(pq)-1);
+    while(pq->m_heap[i] > pq->m_heap[parentNode(i)] && i!=0){
+        //swap i with aprent
+        swap(&pq->m_heap[i], &pq->m_heap[parentNode(i)]);
+        i = parentNode(i);
+    }
+    pq->size++;
 }
 
 int extract_max(pqueue* pq)
 {
     //temp value for largest
-    int maxNum = pq->mHeap[0];
-    pq->mHeap[0] = pq->mHeap[pq->size-1];
+    int maxNum = pq->m_heap[0];
+    //last value in pq is moved to pos 0 in array
+    pq->m_heap[0] = pq->m_heap[size(pq)-1];
+    //decrement size
     pq->size--;
+    //percolate element down
     percolate_down(pq, size(pq), 0);
     return maxNum;
 }
 
 void print_pq(pqueue *pq)
 {
+    if(!size(pq)){
+        printf("Priority Queue Empty!\n");
+        return;
+    }
     for(int i = 0; i < size(pq); i++)
     {
-        printf("mHeap[%d] = %d\n", i, pq->mHeap[i]);
+        printf("m_heap[%d] = %d\n", i, pq->m_heap[i]);
     }
         
 }
 
 void main()
 {
-    pqueue *testQ = create_pq(7);
-    add(testQ,6);
-    add(testQ,8);
-    add(testQ,5);
-    add(testQ,9);
-    add(testQ,1);
-    print_pq(testQ);
-    printf("Max number is %d\n", extract_max(testQ));
-    print_pq(testQ);
-    printf("Max number is %d\n", extract_max(testQ));
-    print_pq(testQ);
+    pqueue* m_heap = create_pq(0);
 
-
-    free(testQ);
+    for(int i = 1; i < 5; i++){
+        add(m_heap, i);
+        print_pq(m_heap);
+        printf("===============================\n");
+    }
+    
+    free(m_heap);
 }
--- Programming Assignment 0/part2/pqueue.h
+++ Programming Assignment 0/part2/pqueue.h
@@ -1,14 +1,17 @@
 typedef struct{
     int size;
-    int mHeap[1000];
+    int m_heap[1000];
 }pqueue;
 
 pqueue* create_pq(int init);
 void swap(int* a, int* b);
-void percolate_up(pqueue* pq, int n, int i);
+int size(pqueue* pq);
+int parent(int i);
+int left(int i);
+int right(int i);
 void percolate_down(pqueue* pq, int n, int i);
 void add(pqueue* pq, int val);
 int extract_max(pqueue* pq);
-int size(pqueue* pq);
+
 void print_pq(pqueue *pq);
 
